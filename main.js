784e6fdd4c1d35ccf7c944c37a7a1a84f2ed5f08
tanvir-ux <tanviralamshuvo@gmail.com>
2020-12-25 21:26:40 +0600

    landing page static image added

---
+++ main.js
@@ -0,0 +1,19 @@
+function HelloWorld(x,y) {
+    if(x > 10) {
+        console.log("print hello world for 10 dollar");
+
+    }
+    else if(x>1 && x<3) {
+        console.log("it's between 1 and 2 dollar");
+    }
+    else console.log("it's somewhere larger then 10 dollar.");
+}
+
+console.log(HelloWorld(10,3));
+
+
+class Node {
+    Node(){
+        
+    }
+}
